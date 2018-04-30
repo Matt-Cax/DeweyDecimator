270c4c02b1788adb9a02ab99f908ec704c5987f1
Peter <peterdevyatkin@gmail.com>
2018-04-29 23:02:40 -0400

    added text box for search results

--- Dewey Decimator/src/deweyDecimator/DD.java
+++ Dewey Decimator/src/deweyDecimator/DD.java
@@ -163,6 +163,7 @@ public class DD extends Application{
 		
 		searchBar.getChildren().addAll(searchGrid, search, radioButtons); // adds searchgrid, search button, and radiobuttons vbox to the searchbar
 		
+		//Results section
 		TextArea searchOutput = new TextArea();
 		searchOutput.setEditable(false);
 		
