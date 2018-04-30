b63563fd1e3a5ea66f9e37d1f7d8961e963cb7bf
anissa <anissa@10.0.2.15>
2018-04-29 17:14:45 -0700

    fixed weird overlapping functions

--- Dewey Decimator/src/deweyDecimator/DD.java
+++ Dewey Decimator/src/deweyDecimator/DD.java
@@ -96,7 +96,6 @@ public class DD extends Application{
 			case SQLManager.PATRON:
 				System.out.println("caught PATRON");
 				stage.setScene(patronView);
-				//System.out.println("SET PATRON");
 				break;
 			case SQLManager.LIBRARIAN:
 				stage.setScene(libView);
@@ -281,7 +280,6 @@ public class DD extends Application{
 		grid.add(search, 2, 1);
 		grid.add(save, 2, 2);
 		
-
 		//TODO: back button?
 		
 		//Button Actions
@@ -303,10 +301,6 @@ public class DD extends Application{
 			
 			// save (SQL)
 			sql.setFines(cn, f);
-		});
-		
-		save.setOnAction(e -> {
-			sql.addFines(Integer.parseInt(cardNumTF.getText()), Double.parseDouble(finesTF.getText()));
 		});
 
 		Scene finesScene = new Scene(grid);
@@ -360,10 +354,6 @@ public class DD extends Application{
 			sql.addUser(fn, ln, ad, pn,"Patron");
 		});
 
-		create.setOnAction(e -> {
-			sql.addPatron(firstNameTF.getText(), lastNameTF.getText(), addressTF.getText(), phoneNumTF.getText());
-		});
-
 		Scene addPatronScene = new Scene(grid);
 		return addPatronScene;
 	}
@@ -395,7 +385,7 @@ public class DD extends Application{
 			stage.setWidth(500);
 			stage.setTitle("Dewey Decimator - Administrator");
 			stage.setScene(adminView);
-			sql.addAdmin(firstNameTF.getText(), lastNameTF.getText());
+
 		});
 
 		Scene addAdminScene = new Scene(grid);
@@ -429,7 +419,6 @@ public class DD extends Application{
 			stage.setWidth(500);
 			stage.setTitle("Dewey Decimator - Administrator");
 			stage.setScene(adminView);
-			sql.addLib(firstNameTF.getText(), lastNameTF.getText());
 		});
 
 		Scene addLibScene = new Scene(grid);
